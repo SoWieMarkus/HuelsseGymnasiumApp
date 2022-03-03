61465be7d53f87f3375dd71024070186c7143b7d
Markus Wieland <wie.markus@web.de>
2022-03-03 12:14:45 +0100

    Add query dashboard endpoint

--- app/src/main/java/markus/wieland/huelssegymnasiumapp/api/SubstitutionAPI.java
+++ app/src/main/java/markus/wieland/huelssegymnasiumapp/api/SubstitutionAPI.java
@@ -8,6 +8,7 @@ import markus.wieland.defaultappelements.api.API;
 import markus.wieland.defaultappelements.api.APIResult;
 import markus.wieland.defaultappelements.api.GetRequest;
 import markus.wieland.defaultappelements.api.RequestResultListener;
+import markus.wieland.huelssegymnasiumapp.api.models.SubstitutionDashboard;
 import markus.wieland.huelssegymnasiumapp.api.models.SubstitutionPlan;
 
 public class SubstitutionAPI extends API {
@@ -27,6 +28,22 @@ public class SubstitutionAPI extends API {
         GetRequest<SubstitutionPlan> routesGetRequest = new GetRequest<>(SubstitutionPlan.class, url, new RequestResultListener<SubstitutionPlan>() {
             @Override
             public void onLoad(SubstitutionPlan response) {
+                notifyClient(response, result);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                notifyClient(null, result);
+            }
+        });
+        routesGetRequest.execute();
+    }
+
+    public void querySubstitutionDashboard(APIResult<SubstitutionDashboard> result) {
+        String url = BASE_URL + "/last";
+        GetRequest<SubstitutionDashboard> routesGetRequest = new GetRequest<>(SubstitutionDashboard.class, url, new RequestResultListener<SubstitutionDashboard>() {
+            @Override
+            public void onLoad(SubstitutionDashboard response) {
                 notifyClient(response, result);
             }
 
